29cf3c1aa78c4c41cf78b1f2ad2d7394487386fe
Bhanu Tejaswini Kaluva <bhanutejaswinikaluva@gmail.com>
2024-08-09 21:04:55 +0530

    adding

--- interpolation-demo--standalone/src/app/app.component.ts
+++ interpolation-demo--standalone/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'interpolation-demo--standalone example';
+  title = 'interpolation-demo--standalone';
 }
